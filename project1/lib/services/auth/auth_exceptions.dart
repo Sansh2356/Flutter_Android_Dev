f9b88663feedafa8421ab5762a3dd8c33c5d754c
Ansh Sharma <sansh2356@gmail.com>
2023-04-14 10:48:34 +0530

    commit till 14th april 10:48 AM

---
+++ project1/lib/services/auth/auth_exceptions.dart
@@ -0,0 +1,16 @@
+///The given are login exceptions///
+class UsernotFoundAuthException implements Exception {}
+
+class WrongPasswordAuthException implements Exception {}
+
+///Register view exception ///
+class WeakPasswordAuthException implements Exception {}
+
+class EmailAlreadyInUseAuthExxception implements Exception {}
+
+class InvalidEmailAuthException implements Exception {}
+
+///generic exceptions///
+class GenericExceptions implements Exception {}
+
+class UserNotLoggedInAuthExceptions implements Exception {}
